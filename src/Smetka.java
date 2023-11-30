5a57a93f221dbad4df645fe7b60200dadd9b8aa6
Filip Mircev <147530688+Zmijaa@users.noreply.github.com>
2023-11-30 09:08:24 +0100

    Add files via upload

---
+++ src/Smetka.java
@@ -0,0 +1,14 @@
+
+public class Smetka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int jab=32;
+		int komp=20;
+		double pras=50;
+		double rezultati=(jab*2)+(komp*5)+(pras*2.5);
+		System.out.println("vkupno vo denari");
+		System.out.println(rezultati);
+	}
+
+}
